d72fc8f25781b8325143c7dc5f479ba864cf6d08
Beth Hansen <beth@>
2017-07-21 10:58:35 -0700

    add 3rd selection with working js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 //business logic
-function Pizza(topping, beverage) {
-  // this.proportion = proportion;
+function Pizza(proportion, topping, beverage) {
+  this.proportion = proportion;
   this.topping = topping;
   this.beverage = beverage;
 }
 
 Pizza.prototype.dinner = function() {
-  return this.topping + this.beverage;
+  return this.proportion + this.topping + this.beverage;
 }
 
 
@@ -15,11 +15,11 @@ $(document).ready(function() {
   $("#orderForm").submit(function(event) {
     event.preventDefault();
 
-    // var inputtedProportion = parseInt($("#proportion").val());
+    var inputtedProportion = parseInt($("#proportion").val());
     var inputtedTopping = parseInt($("#topping").val());
     var inputtedBeverage = parseInt($("#beverage").val());
 // console.log(inputtedBeverage);
-    var newPizza = new Pizza (inputtedTopping, inputtedBeverage);
+    var newPizza = new Pizza (inputtedProportion, inputtedTopping, inputtedBeverage);
 
     $("#order").text(newPizza.dinner());
   });
