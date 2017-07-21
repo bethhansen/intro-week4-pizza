35ae3ca9e8b8b6bcd8691b67a635d586c7163669
Beth <beth97209@gmail.com>
2017-07-20 19:50:05 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,11 @@
 //business logic
 
-function Money(startBalance, deposit, withdraw) {
-  this.startBalance = startBalance;
-  this.deposit = deposit;
-  this.withdraw = withdraw;
-}
 
-Money.prototype Balance = function() {
-  return this.startBalance + this.deposit - this.withdraw;
-}
+
+
+
+
+
 
 
 //user interface logic
-$(document).ready(function() {
-  $("#balance").submit(function(event)) {
-    event.preventDefault();
-
-    var inputtedStartBalance =
-  }
-}
